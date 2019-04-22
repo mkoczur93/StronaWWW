52513b024c32323dd0860320f012ee7efbb11303
mkoczur93 <48414046+mkoczur93@users.noreply.github.com>
2019-04-22 22:15:57 +0200

    Pierwsza wersja

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<title>Portfolio</title>
+	<meta name="description" content="Strona na temat tworzenia stron www">
+	<meta name="keywords" content="zamów, stronę, tworzenie, www, programista, portfolio, php, javascript, html, css, WordPress, Joomla, Drupal">
+	<meta name="author" content="Marcin">
+	<meta http-equiv="X-Ua-Compatible" content="IE=edge">
+	
+	
+	<link rel="stylesheet" href="main.css" type="text/css">
+	
+	
+
+	
+</head>
+
+<body>
+
+	
+
+	
+</body>
+</html>
