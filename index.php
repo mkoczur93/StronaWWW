031c94266f2383c27ac56dc14eb00b2b662d0eec
mkoczur93 <48414046+mkoczur93@users.noreply.github.com>
2019-04-23 16:14:16 +0200

    Tworzenie menu

--- index.php
+++ index.php
@@ -11,18 +11,90 @@
 	<meta name="author" content="Marcin">
 	<meta http-equiv="X-Ua-Compatible" content="IE=edge">
 	
-	
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/fontello.css" type="text/css">
 	<link rel="stylesheet" href="main.css" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
 	
+	<!--[if lt IE 9]>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+	<![endif]-->
 	
-
 	
 </head>
 
 <body>
 
 	
+	<header class="sticky">
+		
+		<nav class="navbar navbar-dark bg-tlonavbar navbar-expand-lg">
+		
+			<a id="link0" class="navbar-brand" href="#kimjestesmy"><i class="icon-picture"></i> <h1 class="logo">StronyWWW.pl</h1></a>
+		
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainmenu" aria-controls="mainmenu" aria-expanded="false" aria-label="Przełącznik nawigacji" id="przelacznik">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+		
+			<div class="collapse navbar-collapse" id="mainmenu">
+			 
+				<ul class="navbar-nav mr-auto">
+				
+					<li class="nav-item" >
+						<a id="link1" class="nav-link" href="#kimjestesmy" >	 O nas </a>
+					</li>
+									
+					<li class="nav-item">
+						<a id="link2" class="nav-link" href="#team" > Team </a>
+					</li>
+					
+					<li class="nav-item">
+						<a id="link3" class="nav-link" href="#cennik"> Cennik </a>
+					</li>
+					
+					<li class="nav-item">
+						<a class="nav-link disabled" title="Dla zalogowanych"> Sklep </a>
+					</li>
+					
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-expanded="false" id="submenu" aria-haspopup="true"> Kontakt </a>
+						
+						<div class="dropdown-menu" aria-labelledby="submenu" >
+						
+							<a id="link5" class="dropdown-item" href="#formularz"> Formularz </a>
+							<a id="link6" class="dropdown-item" href="#jak"> Jak do nas dojechac ? </a>
+							
+						</div>
+						
+					</li>
+				
+				</ul>
+			
+				<form class="form-inline">					
+					
+					<button onclick="location.href='?'" type="button">
+					Zaloguj się</button>
+				
+				</form>
+			
+			
+		</div>
+		</nav>
+		
+	</header>
+	
+	<main>
+		
+			
+		
+	</main>
+	
 
+
+	
+	<script src="jquery-1.11.3.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	
 	
 </body>
 </html>
